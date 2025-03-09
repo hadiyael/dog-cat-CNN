5529ac8d356f78a3dae55694240d9fec9ae47f66
hadiyael <158476408+hadiyael@users.noreply.github.com>
2025-03-09 17:30:21 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import requests
+import gdown
 
+
+@st.cache_resource
+def load_model():
+    url = "https://drive.google.com/1mJCFadc75OkuX5YfM_dbE7lqsWHgEJ7Z"
+    output = "cat_dog_final_model.keras"
+    gdown.download(url, output, quiet=False)
+    
+    model = tf.keras.models.load_model(output)
+    return model
+
+# Charger le modèle une seule fois
+model = load_model()
+
+st.write("Modèle chargé avec succès !")
